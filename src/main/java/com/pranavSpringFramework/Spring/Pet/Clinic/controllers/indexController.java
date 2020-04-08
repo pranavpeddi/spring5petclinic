8ab0e02ebf38a4f540b5f17500290c6112f42780
unknown <pranavped@gmail.com>
2020-04-08 17:27:16 +0530

    added a controller and template file

---
+++ src/main/java/com/pranavSpringFramework/Spring/Pet/Clinic/controllers/indexController.java
@@ -0,0 +1,13 @@
+package com.pranavSpringFramework.Spring.Pet.Clinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class indexController {
+   @RequestMapping({"","/","index","index.html"})
+    public String index()
+    {
+        return "index";
+    }
+}
